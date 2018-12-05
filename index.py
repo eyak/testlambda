fdc61f878522b4181cd017c820ebcc16ecbbbef2
Eyal <eyal@robo-plan.com>
2018-12-05 16:13:42 +0000

    change

--- index.py
+++ index.py
@@ -5,7 +5,9 @@ import datetime
 def handler(event, context):
     data = {
         'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+        'event': event,
+        'context': context
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
