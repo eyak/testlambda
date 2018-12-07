6632484e1fc416c21b4e8683d7a4ba6db23286f7
Eyal <eyal@robo-plan.com>
2018-12-07 13:57:20 +0000

    revert

--- index.py
+++ index.py
@@ -6,8 +6,7 @@ def handler(event, context):
     data = {
         'output': 'Hello World',
         'timestamp': datetime.datetime.utcnow().isoformat(),
-        'params': event.get('multiValueQueryStringParameters', {})#,
-        #'pil': PIL.VERSION
+        'params': event.get('multiValueQueryStringParameters', {})
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
