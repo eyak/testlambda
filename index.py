cc4ccc50d613b80e81b6fde2af52d825f787cd67
Eyal <eyal@robo-plan.com>
2018-12-07 14:19:42 +0000

    noo

--- index.py
+++ index.py
@@ -1,13 +1,12 @@
 import json
 import datetime
-import PIL
+#import PIL
 
 def handler(event, context):
     data = {
         'output': 'Hello World',
         'timestamp': datetime.datetime.utcnow().isoformat(),
-        'params': event.get('multiValueQueryStringParameters', {}),
-        'pil': PIL.VERSION
+        'params': event.get('multiValueQueryStringParameters', {})
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
